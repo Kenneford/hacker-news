9df8df6db228c4de027b06c248c099118dc04a88
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-07-23 22:35:54 +0200

    Stories-header, overview and stories folders created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import FetchData from './FetchData';
 import {useState, useEffect} from "react"
+import FetchData from './FetchData';
+import StoriesHeader from './components/StoriesHeader';
+import Stories from './components/Stories';
 
 
 function App() {
   return(
     <div className='App'>
-        
+        <StoriesHeader />
+        <Stories />
     </div>
   )
 }
---
+++ src/components/StoryOverview.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './StoryOverview.css'
+
+export default function StoryOverview() {
+  return (
+    <div>StoryOverview</div>
+  )
+}
